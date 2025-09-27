a87a65fecc21bf78198dcc37e883e1812e20f962
Abhishek Pise <abhishek13pise@gmail.com>
2025-09-27 23:01:10 +0530

    Fix broken dataset script

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,22 +1,21 @@
+import os
+import shutil
+import kaggle
 
-  import os
-  import shutil
-  import kaggle
+kaggle.api.authenticate()
 
-  kaggle.api.authenticate()
+print(kaggle.api.dataset_list_files('snap/amazon-fine-food-reviews').files)
 
-  print(kaggle.api.dataset_list_files('snap/amazon-fine-food-reviews').files)
+kaggle.api.dataset_download_files('snap/amazon-fine-food-reviews', 'tempo', unzip=True)
 
-  kaggle.api.dataset_download_files('snap/amazon-fine-food-reviews', 'tempo', unzip=True)
+review_csv = './tempo/Reviews.csv'
+new_location = os.path.join(review_csv, '../../..')
 
-  review_csv = './tempo/Reviews.csv'
-  new_location = os.path.join(review_csv, '../../..')
+destination = os.path.abspath(new_location)
 
-  destination = os.path.abspath(new_location)
+shutil.move(review_csv, destination)
 
-  shutil.move(review_csv, destination)
+deletion_folder = 'tempo'
+deletion_path = os.path.join(os.getcwd(), deletion_folder)
 
-  deletion_folder = 'tempo'
-  deletion_path = os.path.join(os.getcwd(), deletion_folder)
-
-  shutil.rmtree(deletion_path)
+shutil.rmtree(deletion_path)
